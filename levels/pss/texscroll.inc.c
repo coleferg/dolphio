dfa2b38fa922159bc723b1aad21b8fbc9ee8c7ae
thecozies <theredcouchroom@gmail.com>
2020-12-21 13:01:13 -0600

    Fixed 3D background in water cube stage

--- levels/pss/texscroll.inc.c
+++ levels/pss/texscroll.inc.c
@@ -1,69 +1,3 @@
-void scroll_pss_dl_SubTopWindow_mesh_vtx_0() {
-	int i = 0;
-	int count = 8;
-	int width = 32 * 0x20;
-	int height = 32 * 0x20;
-
-	static int currentY = 0;
-	int deltaY;
-	Vtx *vertices = segmented_to_virtual(pss_dl_SubTopWindow_mesh_vtx_0);
-
-	deltaY = (int)(0.15000000596046448 * 0x20) % height;
-
-	if (absi(currentY) > height) {
-		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
-	}
-
-	for (i = 0; i < count; i++) {
-		vertices[i].n.tc[1] += deltaY;
-	}
-	currentY += deltaY;
-
-}
-void scroll_pss_dl_SubmarineWater_mesh_vtx_0() {
-	int i = 0;
-	int count = 4;
-	int width = 32 * 0x20;
-	int height = 32 * 0x20;
-
-	static int currentY = 0;
-	int deltaY;
-	Vtx *vertices = segmented_to_virtual(pss_dl_SubmarineWater_mesh_vtx_0);
-
-	deltaY = (int)(0.15000000596046448 * 0x20) % height;
-
-	if (absi(currentY) > height) {
-		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
-	}
-
-	for (i = 0; i < count; i++) {
-		vertices[i].n.tc[1] += deltaY;
-	}
-	currentY += deltaY;
-
-}
-void scroll_pss_dl_SubmarineWindows_mesh_vtx_0() {
-	int i = 0;
-	int count = 8;
-	int width = 32 * 0x20;
-	int height = 32 * 0x20;
-
-	static int currentY = 0;
-	int deltaY;
-	Vtx *vertices = segmented_to_virtual(pss_dl_SubmarineWindows_mesh_vtx_0);
-
-	deltaY = (int)(0.15000000596046448 * 0x20) % height;
-
-	if (absi(currentY) > height) {
-		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
-	}
-
-	for (i = 0; i < count; i++) {
-		vertices[i].n.tc[1] += deltaY;
-	}
-	currentY += deltaY;
-
-}
 void scroll_pss_dl_a_background_mesh_vtx_0() {
 	int i = 0;
 	int count = 117;
@@ -94,10 +28,30 @@ void scroll_pss_dl_a_background_mesh_vtx_0() {
 	currentY += deltaY;
 
 }
+void scroll_pss_dl_rocktubin_001_mesh_vtx_0() {
+	int i = 0;
+	int count = 20;
+	int width = 32 * 0x20;
+	int height = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	Vtx *vertices = segmented_to_virtual(pss_dl_rocktubin_001_mesh_vtx_0);
+
+	deltaX = (int)(0.10000000149011612 * 0x20) % width;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+	}
+	currentX += deltaX;
+
+}
 void scroll_pss() {
-	scroll_pss_dl_SubTopWindow_mesh_vtx_0();
-	scroll_pss_dl_SubmarineWater_mesh_vtx_0();
-	scroll_pss_dl_SubmarineWindows_mesh_vtx_0();
 	scroll_pss_dl_a_background_mesh_vtx_0();
+	scroll_pss_dl_rocktubin_001_mesh_vtx_0();
 
 }
