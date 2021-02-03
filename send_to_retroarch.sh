6ed7f4db68534ef81fd484a94cc8fbdcf4f7d23f
Cole Ferguson <theredcouchroom@gmail.com>
2021-02-03 09:47:23 -0600

    Fixed issues from moving to windows

--- send_to_retroarch.sh
+++ send_to_retroarch.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-/c/Users/there/AppData/Roaming/RetroArch/retroarch.exe -L /c/Users/there/AppData/Roaming/RetroArch/cores/parallel_n64_libretro.dll build/us/sm64.us.f3dex2.z64
+/c/Users/there/AppData/Roaming/RetroArch/retroarch.exe -L /c/Users/there/AppData/Roaming/RetroArch/cores/parallel_n64_libretro.dll build/us/sm64.us.n64.f3dzex.z64
